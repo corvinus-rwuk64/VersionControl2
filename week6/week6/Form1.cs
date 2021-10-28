8b79fe809036779ae8f017d1a366f7d27e582745
PMate <mate.plank@stud.uni-corvinus.hu>
2021-10-28 21:04:30 +0200

    3. + 4. feladat

--- week6/week6/Form1.cs
+++ week6/week6/Form1.cs
@@ -7,21 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week6.Entities;
+using week6.ServiceReference1;
 
 namespace week6
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
 
+            webszolgaltatasHivas();
 
+            dataGridView1.DataSource = Rates;
         }
 
         public void webszolgaltatasHivas()
         {
-            var mnbService = new 
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
