08caa9257d9b129219348393d7620fe73bbfdc92
PMate <mate.plank@stud.uni-corvinus.hu>
2021-10-28 21:24:52 +0200

    7. feladat

--- week6/week6/Form1.cs
+++ week6/week6/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week6.Entities;
 using week6.ServiceReference1;
@@ -21,11 +22,7 @@ namespace week6
         {
             InitializeComponent();
 
-            //webszolgaltatasHivas();
-
-            dataGridView1.DataSource = Rates;
-
-            xmlFeldolgozas();
+            RefreshData();
         }
 
         public string webszolgaltatasHivas()
@@ -34,9 +31,9 @@ namespace week6
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
@@ -67,6 +64,53 @@ namespace week6
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+        }
+
+        public void diagramFeltoltes()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        public void RefreshData()
+        {
+            Rates.Clear();
+
+            //webszolgaltatasHivas();
+
+            dataGridView1.DataSource = Rates;
+
+            xmlFeldolgozas();
+
+            diagramFeltoltes();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
