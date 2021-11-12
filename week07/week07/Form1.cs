1766bae73d0d303974ee5d34b146d5bb83f4395d
PMate <mate.plank@stud.uni-corvinus.hu>
2021-11-12 21:48:37 +0100

    Termékválasztó gombok

--- week07/week07/Form1.cs
+++ week07/week07/Form1.cs
@@ -20,8 +20,14 @@ namespace week07
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set
+            { 
+                _factory = value;
+                DisplayNext();
+            }
         }
+
+        private Toy _nextToy;
 
         public Form1()
         {
@@ -53,6 +59,25 @@ namespace week07
                 mainPanel.Controls.Remove(oldestBall);
                 _toys.Remove(oldestBall);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null) Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
         }
     }
 }
